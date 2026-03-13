9ffb7df2d5c11acdb7f9880203db47ba639c8ffb
mmmnz-ii <mingzzi0122@gmail.com>
2026-03-13 10:59:18 +0900

    Add files via upload

--- js/finca_script.js
+++ js/finca_script.js
@@ -3,7 +3,7 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", ()=>{
-    if(window.scrollY > 120){
+    if(window.scrollY > 80){
         header.classList.add("fixed");
     }else{
         header.classList.remove("fixed");
